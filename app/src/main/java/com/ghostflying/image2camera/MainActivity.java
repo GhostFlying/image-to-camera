7a40e93063047e297b1233da17c8b745d4858abb
GhostFlying <ghostflying@gmail.com>
2017-02-19 15:05:39 +0800

    update build tools and remove ingress related function

--- app/src/main/java/com/ghostflying/image2camera/MainActivity.java
+++ app/src/main/java/com/ghostflying/image2camera/MainActivity.java
@@ -34,80 +34,35 @@ import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 
-public class MainActivity extends AppCompatActivity implements BaseAlertDialogFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
     private static final int PICK_IMAGE = 10;
     private static final String IMAGE_TYPE = "image/*";
-    private static final String INGRESS_PACKAGE_NAME = "com.nianticproject.ingress";
 
     private Uri outputUri;
     private List<AppInfo> cameraApps;
-
-    @Bind(R.id.app_choose)
-    View mAppChoose;
-    @Bind(R.id.default_app)
-    TextView mDefaultApp;
-    @Bind(R.id.checkbox_only_work_for_ingress)
-    CheckBox mWorkForIngress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        loadDefaultApp();
-        loadWorkForIngress();
         Intent cameraIntent = getIntent();
-        if (cameraIntent.getAction().equals(MediaStore.ACTION_IMAGE_CAPTURE)){
-            if (INGRESS_PACKAGE_NAME.equals(getCallingPackage())){
-                startPickImages(cameraIntent);
-            }
-            else {
-                SharedPreferences preferences = getSharedPreferences(SettingUtil.SETTING_NAME, MODE_PRIVATE);
-
-                if (preferences.getBoolean(SettingUtil.ONLY_WORK_FOR_INGRESS, SettingUtil.DEFAULT_ONLY_WORK_FOR_INGRESS)){
-                    String packageName = preferences.getString(SettingUtil.DEFAULT_CAMERA_APP, null);
-                    String activityName = preferences.getString(SettingUtil.DEFAULT_CAMERA_APP_ACTIVITY, null);
-                    if (packageName == null || activityName == null){
-                        Toast.makeText(this, R.string.toast_please_set_default_camera_app, Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        ComponentName componentName = new ComponentName(packageName, activityName);
-                        cameraIntent.setComponent(componentName);
-                        cameraIntent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
-                        startActivity(cameraIntent);
-                        finish();
-                    }
-                }
-                else {
-                    startPickImages(cameraIntent);
-                }
-            }
+        if (cameraIntent.getAction().equals(MediaStore.ACTION_IMAGE_CAPTURE)) {
+            startPickImages(cameraIntent);
         }
     }
 
     private void startPickImages(Intent cameraIntent) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             ClipData clipData = cameraIntent.getClipData();
             outputUri = clipData.getItemAt(0).getUri();
         }
 
         // compatibility for system below lollipop
-        if (outputUri == null){
+        if (outputUri == null) {
             Bundle extra = cameraIntent.getExtras();
             outputUri = extra.getParcelable(MediaStore.EXTRA_OUTPUT);
         }
         pickImages();
-    }
-
-    @OnClick(R.id.app_choose)
-    public void chooseApp(){
-        new LoadAppTask().execute();
-    }
-
-    @OnCheckedChanged(R.id.checkbox_only_work_for_ingress)
-    public void onlyForIngressChanged(boolean checked){
-        SharedPreferences preferences = getSharedPreferences(SettingUtil.SETTING_NAME, MODE_PRIVATE);
-        preferences.edit().putBoolean(SettingUtil.ONLY_WORK_FOR_INGRESS, checked).apply();
     }
 
     private void pickImages() {
@@ -117,14 +72,14 @@ public class MainActivity extends AppCompatActivity implements BaseAlertDialogFr
         startActivityForResult(Intent.createChooser(intent, getString(R.string.select_image)), PICK_IMAGE);
     }
 
-    private void saveFile(Uri source, Uri des){
-        try{
+    private void saveFile(Uri source, Uri des) {
+        try {
             final InputStream inputStream = getContentResolver().openInputStream(source);
             final File outputFile = new File(des.getPath());
             final OutputStream outputStream = new FileOutputStream(outputFile);
 
-            try{
-                try{
+            try {
+                try {
                     final byte[] buffer = new byte[1024];
                     int read;
 
@@ -135,22 +90,19 @@ public class MainActivity extends AppCompatActivity implements BaseAlertDialogFr
 
                     // copy images successfully.
                     setResult(Activity.RESULT_OK);
-                }
-                finally {
+                } finally {
                     inputStream.close();
                     outputStream.close();
                 }
-            }
-            catch (IOException e){
+            } catch (IOException e) {
                 handleException(e);
             }
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             handleException(e);
         }
     }
 
-    private void handleException(Exception e){
+    private void handleException(Exception e) {
         e.printStackTrace();
         runOnUiThread(new Runnable() {
             @Override
@@ -160,12 +112,12 @@ public class MainActivity extends AppCompatActivity implements BaseAlertDialogFr
         });
     }
 
-    private void deleteFile(Uri file){
+    private void deleteFile(Uri file) {
         File outputFile = new File(file.getPath());
         outputFile.delete();
     }
 
-    private void handleNotSelect(){
+    private void handleNotSelect() {
         Toast.makeText(this, R.string.not_select_toast, Toast.LENGTH_SHORT).show();
         deleteFile(outputUri);
         finish();
@@ -180,45 +132,14 @@ public class MainActivity extends AppCompatActivity implements BaseAlertDialogFr
                 setResult(RESULT_CANCELED);
                 return;
             }
-            if (resultCode == Activity.RESULT_OK){
+            if (resultCode == Activity.RESULT_OK) {
                 new SaveFileTask().execute(data.getData(), outputUri);
                 setResult(RESULT_OK);
             }
         }
     }
 
-    @Override
-    public void onPositiveButtonClick(int value, int title) {
-        if (cameraApps != null){
-            SharedPreferences preferences = getSharedPreferences(SettingUtil.SETTING_NAME, MODE_PRIVATE);
-            if (value < cameraApps.size()){
-                AppInfo app = cameraApps.get(value);
-                preferences.edit()
-                        .putString(SettingUtil.DEFAULT_CAMERA_APP_ACTIVITY, app.activityName)
-                        .putString(SettingUtil.DEFAULT_CAMERA_APP_NAME, app.appName)
-                        .putString(SettingUtil.DEFAULT_CAMERA_APP, app.packageName)
-                        .apply();
-                loadDefaultApp();
-            }
-        }
-    }
-
-    @Override
-    public void onNegativeButtonClick(int value, int title) {
-
-    }
-
-    private void loadDefaultApp(){
-        SharedPreferences preferences = getSharedPreferences(SettingUtil.SETTING_NAME, MODE_PRIVATE);
-        mDefaultApp.setText(preferences.getString(SettingUtil.DEFAULT_CAMERA_APP_NAME, getString(R.string.click_to_choose)));
-    }
-
-    private void loadWorkForIngress(){
-        SharedPreferences preferences = getSharedPreferences(SettingUtil.SETTING_NAME, MODE_PRIVATE);
-        mWorkForIngress.setChecked(preferences.getBoolean(SettingUtil.ONLY_WORK_FOR_INGRESS, SettingUtil.DEFAULT_ONLY_WORK_FOR_INGRESS));
-    }
-
-    private class SaveFileTask extends AsyncTask<Uri, Void, Void>{
+    private class SaveFileTask extends AsyncTask<Uri, Void, Void> {
         DialogFragment dialogFragment;
 
         @Override
@@ -234,13 +155,13 @@ public class MainActivity extends AppCompatActivity implements BaseAlertDialogFr
         }
 
         @Override
-        protected void onPostExecute(Void result){
+        protected void onPostExecute(Void result) {
             dialogFragment.dismiss();
             finish();
         }
     }
 
-    private class LoadAppTask extends AsyncTask<Void, Void, Void>{
+    private class LoadAppTask extends AsyncTask<Void, Void, Void> {
         private DialogFragment dialogFragment;
         private ArrayList<String> appNames;
         private int checked;
@@ -262,8 +183,8 @@ public class MainActivity extends AppCompatActivity implements BaseAlertDialogFr
 
             cameraApps = new ArrayList<>(pkgAppsList.size());
             appNames = new ArrayList<>(pkgAppsList.size());
-            for (ResolveInfo each : pkgAppsList){
-                if (!each.activityInfo.packageName.equals(BuildConfig.APPLICATION_ID)){
+            for (ResolveInfo each : pkgAppsList) {
+                if (!each.activityInfo.packageName.equals(BuildConfig.APPLICATION_ID)) {
                     AppInfo info = new AppInfo();
                     info.appName = each.loadLabel(packageManager).toString();
                     info.activityName = each.activityInfo.name;
@@ -271,7 +192,7 @@ public class MainActivity extends AppCompatActivity implements BaseAlertDialogFr
                     cameraApps.add(info);
                     appNames.add(info.appName);
 
-                    if (defaultApp != null && info.activityName.equals(defaultApp)){
+                    if (defaultApp != null && info.activityName.equals(defaultApp)) {
                         checked = cameraApps.size() - 1;
                     }
                 }
@@ -287,7 +208,7 @@ public class MainActivity extends AppCompatActivity implements BaseAlertDialogFr
         }
     }
 
-    private static class AppInfo{
+    private static class AppInfo {
         String packageName;
         String activityName;
         String appName;
--- app/src/main/java/com/ghostflying/image2camera/SettingUtil.java
+++ app/src/main/java/com/ghostflying/image2camera/SettingUtil.java
@@ -5,10 +5,7 @@ package com.ghostflying.image2camera;
  */
 public abstract class SettingUtil {
     public static String SETTING_NAME = "Settings";
-    public static String ONLY_WORK_FOR_INGRESS = "OnlyWorkForIngress";
     public static String DEFAULT_CAMERA_APP = "DefaultCameraApp";
     public static String DEFAULT_CAMERA_APP_ACTIVITY = "DefaultCameraAppActivity";
     public static String DEFAULT_CAMERA_APP_NAME = "DefaultCameraAppName";
-
-    public static boolean DEFAULT_ONLY_WORK_FOR_INGRESS = false;
 }
